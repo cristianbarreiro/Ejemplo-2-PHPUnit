32ed272d2d2566f27b8ee5a768d9b72d2ab74167
Cristian <cb2onix@gmail.com>
2025-05-31 23:48:36 -0300

    feat: data provider for PHPUnit 10+

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -3,34 +3,26 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use App\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
-    public function testCuandoLePaso3DevuelveFizz()
+    public static function casosDeUso(): array
     {
-        $fizzBuzz = new \App\FizzBuzz();
-        $resultado = $fizzBuzz->diNumero(3);
-        $this->assertEquals('Fizz', $resultado);
+        return [
+            [3, 'Fizz'],
+            [5, 'Buzz'],
+            [15, 'FizzBuzz'],
+            [1, '1']
+        ];
     }
 
-    public function testCuandoLePaso5DevuelveBuzz()
+    #[DataProvider('casosDeUso')]
+    public function testParaFizzBuzz($numeroATestear, $resultadoEsperado)
     {
-        $fizzBuzz = new \App\FizzBuzz();
-        $resultado = $fizzBuzz->diNumero(5);
-        $this->assertEquals('Buzz', $resultado);
-    }
-
-    public function testCuandoLePaso3y5DevuelveFizzBuzz()
-    {
-        $fizzBuzz = new \App\FizzBuzz();
-        $resultado = $fizzBuzz->diNumero(15);
-        $this->assertEquals('FizzBuzz', $resultado);
-    }
-
-    public function testCuandoLeDoyElNroUnoDevuelveUno()
-    {
-        $fizzBuzz = new \App\FizzBuzz();
-        $resultado = $fizzBuzz->diNumero(1);
-        $this->assertEquals('1', $resultado);
+        $fizzBuzz = new FizzBuzz();
+        $resultado = $fizzBuzz->diNumero($numeroATestear);
+        $this->assertEquals($resultadoEsperado, $resultado);
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-feature/dataprovider-test',
+        'version' => 'dev-feature/dataprovider-test',
+        'reference' => 'db89a811f9a333f3541e9c403cad590f718e542f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-feature/dataprovider-test',
+            'version' => 'dev-feature/dataprovider-test',
+            'reference' => 'db89a811f9a333f3541e9c403cad590f718e542f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
