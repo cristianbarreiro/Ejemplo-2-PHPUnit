e380ae6585b2d95864ddcb813013d22ad3031bd8
Cristian <cb2onix@gmail.com>
2025-05-31 23:00:28 -0300

    feat: testear devolución de Buzz & FizzBuzz

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -12,4 +12,18 @@ class FizzBuzzTest extends TestCase
         $resultado = $fizzBuzz->diNumero(3);
         $this->assertEquals('Fizz', $resultado);
     }
+
+    public function testCuandoLePaso5DevuelveBuzz()
+    {
+        $fizzBuzz = new \App\FizzBuzz();
+        $resultado = $fizzBuzz->diNumero(5);
+        $this->assertEquals('Buzz', $resultado);
+    }
+
+    public function testCuandoLePaso3y5DevuelveFizzBuzz()
+    {
+        $fizzBuzz = new \App\FizzBuzz();
+        $resultado = $fizzBuzz->diNumero(15);
+        $this->assertEquals('FizzBuzz', $resultado);
+    }
 }
