bdc927f99ae012ee08f2924f7c09b7d8e3f102ff
Cristian <cb2onix@gmail.com>
2025-06-03 11:39:37 -0300

    feat: agregamos setUp
    con esta función podemos evitar el código repetido

--- tests/unit/QueRopaOfertarTest.php
+++ tests/unit/QueRopaOfertarTest.php
@@ -4,105 +4,79 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use App\QueRopaOfertar;
 use App\TiempoApi;
+use PHPUnit\Framework\MockObject\MockObject;
 
 #[CoversClass(QueRopaOfertar::class)]
 class QueRopaOfertarTest extends TestCase
 {
+    /** @var TiempoApi&MockObject */
+    private TiempoApi $tiempoApi;
+    private QueRopaOfertar $queRopaOfertar;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->tiempoApi = $this->createMock(TiempoApi::class);
+        $this->queRopaOfertar = new QueRopaOfertar($this->tiempoApi);
+    }
+
     public function testDeterminaCamisetasCuandoHaceMasDe18Grados()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(25.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Camisetas', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(25.0);
+        $this->assertEquals('Camisetas', $this->queRopaOfertar->determina('Madrid'));
     }
 
     public function testDeterminaCamisasCuandoHaceEntre10y18Grados()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(15.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Camisas', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(15.0);
+        $this->assertEquals('Camisas', $this->queRopaOfertar->determina('Madrid'));
     }
 
     public function testDeterminaAbrigosCuandoHaceMenosDe10Grados()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(5.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Abrigos', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(5.0);
+        $this->assertEquals('Abrigos', $this->queRopaOfertar->determina('Madrid'));
     }
-
-    // CASOS LÍMITE -----------------------------------
 
     public function testDeterminaCamisasCuandoHaceExactamente10Grados()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(10.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Camisas', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(10.0);
+        $this->assertEquals('Camisas', $this->queRopaOfertar->determina('Madrid'));
     }
 
     public function testDeterminaCamisasCuandoHaceExactamente18Grados()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(18.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Camisas', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(18.0);
+        $this->assertEquals('Camisas', $this->queRopaOfertar->determina('Madrid'));
     }
-
-    // VALORES EXTREMOS --------------------------------
 
     public function testDeterminaAbrigosCuandoHaceTemperaturaNegativa()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(-5.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Abrigos', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(-5.0);
+        $this->assertEquals('Abrigos', $this->queRopaOfertar->determina('Madrid'));
     }
 
     public function testDeterminaCamisetasCuandoHaceTemperaturaMuyAlta()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(45.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Camisetas', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(45.0);
+        $this->assertEquals('Camisetas', $this->queRopaOfertar->determina('Madrid'));
     }
-
-    // MANEJO DE ERRORES --------------------------------
 
     public function testDeterminaConTemperaturaNula()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->willReturn(-999.0);
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Datos insuficientes', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->willReturn(-999.0);
+        $this->assertEquals('Datos insuficientes', $this->queRopaOfertar->determina('Madrid'));
     }
-
 
     public function testDeterminaCuandoLaApiLanzaExcepcion()
     {
-        $apiMock = $this->createMock(TiempoApi::class);
-        $apiMock->method('queTemperaturaHaceEn')->will($this->throwException(new \Exception("API no responde")));
-
-        $ropa = new QueRopaOfertar($apiMock);
-        $this->assertEquals('Datos insuficientes', $ropa->determina('Madrid'));
+        $this->tiempoApi->method('queTemperaturaHaceEn')->will($this->throwException(new \Exception("API no responde")));
+        $this->assertEquals('Datos insuficientes', $this->queRopaOfertar->determina('Madrid'));
     }
 
     public function testDeterminaLanzaExcepcionCuandoCiudadVacia()
     {
         $this->expectException(\InvalidArgumentException::class);
-
-        $apiMock = $this->createMock(TiempoApi::class);
-        $ropa = new QueRopaOfertar($apiMock);
-
-        $ropa->determina('');
+        $this->queRopaOfertar->determina('');
     }
 }
